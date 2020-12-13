d92fdb7d62ee8415bb74b3a5359f34b9627ef577
Admin <Admin@LAPTOP-L20IRP9S>
2020-12-13 23:18:29 +0700

    Init Repository

---
+++ webdriver/api/Topic_01_Check_environment.java
@@ -0,0 +1,55 @@
+package api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_environment {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+
+	@Test
+	public void TC_04_Check() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+	
+	@Test
+	public void TC_05_Check() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+	
+	@Test
+	public void TC_06_Check() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
